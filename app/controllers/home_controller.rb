905b5293f47517495d2c5fcccbd42a74ba8d55c6
gyeol1212 <gyeol1212@likelion.org>
2018-08-04 20:11:43 +0900

    edit 수정하면 db 저장

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -144,7 +144,8 @@ class HomeController < ApplicationController
       <brand>#{params[:brand]}</brand>
 
       # 대표 이미지 - 300x300 이상, jpg, jpeg, png만
-      #{prdImages}
+      <prdImage01>https://picsum.photos/300/300</prdImage01>
+      
       <htmlDetail>
         <![CDATA[
           #{params[:htmlDetail]}
@@ -353,10 +354,10 @@ class HomeController < ApplicationController
 
     # list로 기본적인 상품정보를 얻어온 후 params로 넘겨줘야 기존 edit form 채워줄수 있음
     @product = Product.where(prdNo: params[:prdNo].to_i)[0]
-    @prdImages = @product.photos
-    for i in @prdImages.length...5
-      @prdImages.push[nil]
-    end
+    # @prdImages = @product.photos
+    # for i in @prdImages.length...5
+    #   @prdImages.push[nil]
+    # end
     # @product_prd = eval(@product.prd)
     @product_option = eval(@product.option)
 
@@ -378,6 +379,8 @@ class HomeController < ApplicationController
 
     productOption = {}
     options = ""
+
+  
     if params[:colValue] == true
 
 
@@ -571,7 +574,7 @@ class HomeController < ApplicationController
     </ProductNotification>
 
     #판매자 상품코드
-    <sellerPrdCd>selload1212</sellerPrdCd>          
+    <sellerPrdCd>#{current_user.email.split("@")[0] + '_selload'}</sellerPrdCd>          
         
 
     
@@ -579,6 +582,12 @@ class HomeController < ApplicationController
 
     
   </Product>"
+
+    updateProduct = Product.where(prdNo: params[:prdNo].to_i)[0]
+    updateProduct.update(product_params)
+    updateProduct.save
+
+    redirect_to home_list_path
 
     response = http.request(request)
     puts request.body
